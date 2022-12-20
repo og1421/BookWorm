c3fe8d453d0facd5d7d30c8ed6466d734d6f1bf7
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2022-12-20 19:04:32 -0300

    Initial Commit

---
+++ BookWorm/BookWormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormApp.swift
+//  BookWorm
+//
+//  Created by Orlando Moraes Martins on 20/12/22.
+//
+
+import SwiftUI
+
+@main
+struct BookWormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
