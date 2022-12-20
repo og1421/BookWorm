b58d78c469599ad70b384576edca479ae10f2903
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2022-12-20 19:09:51 -0300

    almost finish

---
+++ BookWorm/Model/DataControler.swift
@@ -0,0 +1,8 @@
+//
+//  DataControler.swift
+//  BookWorm
+//
+//  Created by Orlando Moraes Martins on 20/12/22.
+//
+
+import Foundation
---
+++ BookWorm/Views/EmojiRatingView.swift
@@ -0,0 +1,34 @@
+//
+//  EmojiRatingView.swift
+//  BookWorm
+//
+//  Created by Orlando Moraes Martins on 20/12/22.
+//
+
+import SwiftUI
+
+struct EmojiRatingView: View {
+    let rating: Int16
+    
+    var body: some View {
+        switch rating {
+        case 1:
+            return Text("1")
+        case 2:
+            return Text("2")
+        case 3:
+            return Text("3")
+        case 4:
+            return Text("4")
+        default:
+            return Text("5")
+        }
+    }
+}
+
+struct EmojiRatingView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmojiRatingView(rating: 3)
+    }
+}
+
